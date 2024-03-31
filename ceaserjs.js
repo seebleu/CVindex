e54fc486d03467ebe392fcd73ec3eaf2f05d2a2f
seebleu <152238738+seebleu@users.noreply.github.com>
2024-03-31 18:44:50 -0400

    Java ceasers party

---
+++ ceaserjs.js
@@ -0,0 +1,6 @@
+let partyLocation = "GARDEN"; // String - message might change
+const shiftValue = 3; // Number - shift value remains constant through the process
+Number.isInteger(shiftValue);
+let encrpytedMessage = ""; // String - this will change we message reveals the orginal party
+let decryptedMessage = ""; // String - this will change when the message reveals the party location
+let isPartySafe = false; // String - Hopefully this doesnt channge 
